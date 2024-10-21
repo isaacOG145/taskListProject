1db0b1785b378da9f58553deeb488cdf60f817d1
isaacOG145 <20233tn145@utez.edu.mx>
2024-10-20 20:59:20 -0600

    Subir el repositorio

---
+++ src/main/java/utez/edu/_b/taskListProject/TaskListProjectApplication.java
@@ -0,0 +1,13 @@
+package utez.edu._b.taskListProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskListProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskListProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/utez/edu/_b/taskListProject/TaskListProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu._b.taskListProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskListProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
